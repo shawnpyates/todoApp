c1478c2562887c1f3e0d883b285d7859a63b5dee
peachy19 <prasg19@gmail.com>
2017-04-27 22:01:54 +0000

    Adds userinput to Movies list, also modified CSS/Style

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,34 @@
 $(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
+  // $.ajax({
+  //   method: "GET",
+  //   url: "/api/users"
+  // }).done((users) => {
+  //   for(user of users) {
+  //     $("<div>").text(user.name).appendTo($("body"));
+  //   }
+  // });;
+$('.listgroup').sortable({
+    connectWith: '.listgroup',
+    placeholder: 'placeholder',
+  });
+$(".add").on("click", function(event) {
+//  console.log("field", $(".form-control"));
+  const userInput = $(".form-control")[0].value;
+  $(".form-control")[0].value = "";
+
+// console.log("Input", $userInput);
+renderTask(userInput);
+});
+
+function createElement(input){
+  const $taskButton = `<button type="button" class="list-group-item">${input}</button>`
+  return $taskButton;
+}
+
+function renderTask(input){
+
+  $(".movies").append( createElement(input));
+}
+
+
 });
