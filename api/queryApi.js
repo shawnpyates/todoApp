b9a1a4e4c79434e084af4f020a8e803b8106fb32
Shawn Yates <yates.shawnp@gmail.com>
2017-04-29 03:47:37 +0000

    Categorize search result

--- api/queryApi.js
+++ api/queryApi.js
@@ -1,11 +1,12 @@
 const arg = process.argv.slice(2);
 const searchItem = arg.join(" ");
-const https = require('https');
-const apiTypes = require('./apiTypes');
+const https = require("https");
+const apiTypes = require("./apiTypes");
 
 
 
 function generateQueryUrl(searchID, item) {
+  // console.log(`https://www.googleapis.com/customsearch/v1?key=${apiTypes.apiKey}&cx=${searchID}&q=${item}`);
   return `https://www.googleapis.com/customsearch/v1?key=${apiTypes.apiKey}&cx=${searchID}&q=${item}`;
 }
 
@@ -50,35 +51,81 @@ function getSplitMessage(type, cb) {
   });
 }
 
-// since most movies are sold by Walmart, we will wait for queries to go through
-// IMDB first, and if a match is found, Walmart will not be searched
-// this way, when a movie title is searched, it will go into the "movies to watch" category
-getSplitMessage(apiTypes.imdb, function(splitMessages){
-  if (splitMessages[0].toLowerCase() === `${searchItem} `.toLowerCase()) {
-    apiTypes.imdb.found = true;
+// find if search item has matches in API for each search type
+function findInApi() {
+  getSplitMessage(apiTypes.imdb, function(splitMessages){
+    if (splitMessages[0].toLowerCase() === `${searchItem} `.toLowerCase()) {
+      apiTypes.imdb.found = true;
+    } else {
+      apiTypes.imdb.found = false;
+    }
+  });
+  getSplitMessage(apiTypes.gBooks, function(splitMessages){
+    if (splitMessages[0].toLowerCase() === `${searchItem} `.toLowerCase()) {
+      apiTypes.gBooks.found = true;
+    } else {
+      apiTypes.gBooks.found = false;
+    }
+  });
+  getSplitMessage(apiTypes.zomato, function(splitMessages){
+    if (splitMessages[0].toLowerCase() === `${searchItem}`.toLowerCase() ||
+        splitMessages[1].toLowerCase() === `${searchItem} `.toLowerCase()) {
+      apiTypes.zomato.found = true;
+    } else {
+      apiTypes.zomato.found = false;
+    }
+  });
+  getSplitMessage(apiTypes.walmart, function(splitMessages){
+    if (splitMessages[0].toLowerCase().includes(searchItem.toLowerCase()) &&
+        splitMessages[0] !== "Walmart") {
+      apiTypes.walmart.found = true;
+    } else {
+      apiTypes.walmart.found = false;
+    }
+  });
+}
+
+// allow time for each search and collect values before displaying result to user
+function setValues() {
+  return new Promise((resolve, reject) => {
+    findInApi();
+    setTimeout(() => resolve ([apiTypes.imdb.found, apiTypes.gBooks.found, apiTypes.zomato.found, apiTypes.walmart.found]), 1000);
+  });
+}
+
+// assign a category to each take so we can result how to display the result to the user
+function finish(results) {
+  let imdb = results[0];
+  let gBooks = results[1];
+  let zomato = results[2];
+  let walmart = results[3];
+  if (imdb && !gBooks && !zomato) {
+    console.log(1);
+    return 1;
+  } else if (!imdb && gBooks && !zomato) {
+    console.log(2);
+    return 2;
+  } else if (!imdb && !gBooks && zomato) {
+    console.log(3);
+    return 3;
+  } else if (!imdb && !gBooks && !zomato) {
+    if (walmart) {
+      console.log(4);
+      return 4;
+    }
+  } else if (imdb && gBooks && !zomato) {
+    console.log(5);
+    return 5;
   } else {
-      getSplitMessage(apiTypes.walmart, function(splitMessages){
-        if (splitMessages[0].toLowerCase().includes(searchItem.toLowerCase()) &&
-            splitMessages[0] !== "Walmart") {
-            apiTypes.walmart.found = true;
-        }
-        console.log("WALMART: ", apiTypes.walmart.found)
-      });
+    console.log(6);
+    return 6;
   }
-  console.log("IMDB: ", apiTypes.imdb.found);
-});
+}
 
-getSplitMessage(apiTypes.gBooks, function(splitMessages){
-  if (splitMessages[0].toLowerCase() === `${searchItem} `.toLowerCase()) {
-    apiTypes.gBooks.found = true;
-  }
-  console.log("GBOOKS: ", apiTypes.gBooks.found);
-});
 
-getSplitMessage(apiTypes.zomato, function(splitMessages){
-  if (splitMessages[0].toLowerCase() === `${searchItem}`.toLowerCase() ||
-      splitMessages[1].toLowerCase() === `${searchItem} `.toLowerCase()) {
-    apiTypes.zomato.found = true;
-  }
-  console.log("ZOMATO: ", apiTypes.zomato.found);
-});
+setValues().then(finish);
+
+
+
+
+
