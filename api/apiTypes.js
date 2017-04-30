073db61b5def91cecece5d0d261ff92181c5a1b0
Shawn Yates <yates.shawnp@gmail.com>
2017-04-30 01:38:44 +0000

    reset key and id values, filtering out nonsense results

--- api/apiTypes.js
+++ api/apiTypes.js
@@ -1,37 +1,82 @@
+// module.exports = {
+
+// apiKey: "AIzaSyDdE5hPaeswzRcV0qmWuXjuc9fjyeuCtbk",
+
+// imdb: {
+//   id: "014944931791551336491:y_4vmq2daeg",
+//   splitters: ["("],
+//   // message: "",
+//   // splitMessages: [],
+//   found: false
+// },
+
+// gBooks: {
+//   id: "014944931791551336491:0fcbghes1lg",
+//   splitters: ["-"],
+//   // message: "",
+//   // splitMessages: [],
+//   found: false
+// },
+
+// zomato: {
+//   id: "014944931791551336491:vc3gfylldwc",
+//   splitters: [",", "-"],
+//   // message: "",
+//   // splitMessages: [],
+//   found: false
+// },
+
+// walmart: {
+//   id: "014944931791551336491:v4oku8mibiw",
+//   splitters: [","],
+//   // message: "",
+//   // splitMessages: [],
+//   found: false
+// }
+
+// };
+
 module.exports = {
 
-apiKey: "AIzaSyDdE5hPaeswzRcV0qmWuXjuc9fjyeuCtbk",
+//apiKey: "AIzaSyDdE5hPaeswzRcV0qmWuXjuc9fjyeuCtbk",
+apiKey: "AIzaSyBg2zp4e0vgvzVLu37tJs_KUI7Nq5LlFmI",
 
 imdb: {
-  id: "014944931791551336491:y_4vmq2daeg",
-  splitters: ["("],
-  // message: "",
-  // splitMessages: [],
-  found: false
+ //id: "014944931791551336491:y_4vmq2daeg",
+ id: "014978102484720330182:42g1mqyd5yk",
+ splitters: ["("],
+ // message: "",
+ // splitMessages: [],
+ found: false
 },
 
 gBooks: {
-  id: "014944931791551336491:0fcbghes1lg",
-  splitters: ["-"],
-  // message: "",
-  // splitMessages: [],
-  found: false
+
+ id: "014944931791551336491:0fcbghes1lg",
+ //id: "014978102484720330182:aqysccdui08",
+
+ splitters: ["-"],
+ // message: "",
+ // splitMessages: [],
+ found: false
 },
 
 zomato: {
-  id: "014944931791551336491:vc3gfylldwc",
-  splitters: [",", "-"],
-  // message: "",
-  // splitMessages: [],
-  found: false
+ id: "014978102484720330182:4xbqnb9rrts",
+ //id: "014944931791551336491:vc3gfylldwc",
+ splitters: [",", "-"],
+ // message: "",
+ // splitMessages: [],
+ found: false
 },
 
 walmart: {
-  id: "014944931791551336491:v4oku8mibiw",
-  splitters: [","],
-  // message: "",
-  // splitMessages: [],
-  found: false
+ id: "014978102484720330182:ybotzlybqb8",
+ //id: "014944931791551336491:v4oku8mibiw",
+ splitters: [","],
+ // message: "",
+ // splitMessages: [],
+ found: false
 }
 
 };
