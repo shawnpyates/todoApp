c93439b1e7e6ce8cbf46950c1d0d67a04b9147cc
peachy19 <prasg19@gmail.com>
2017-05-01 20:08:37 +0000

    Refactored, removed lint errors

--- public/scripts/clickEdit.js
+++ public/scripts/clickEdit.js
@@ -3,24 +3,5 @@ $(() => {
   $("body").on("click", ".glyphicon.glyphicon-pencil", function(event){
     event.stopImmediatePropagation();
     console.log("Edited list item id", $(this).parent());
-  //     $(this).parent().hide();
-  //     $('input[type="text"]').show().focus();
-  //   $.ajax({
-  //     url: "/tasks/"+$(this).parent().data('task-id')+"?_method=DELETE",
-  //     type: "DELETE",
-  //     success: () => {
-  //       $(this).parent().remove();
-  //     }
-  //   });
   });
-
-  // $(".modal-footer .btn-primary").on("click",function(event){
-  //     $input = $(this).parent().sibling().find(".edit-input");
-  //     console.log("Input",$input);
-  //     $input.text
-  //     $input.hide();
-  //       dad.find('label').show();
-  // })
-
-
 });
--- public/scripts/clickListItem.js
+++ public/scripts/clickListItem.js
@@ -1,17 +1,10 @@
+//adds content to the modal when clicked
 $(() => {
     $("body").on("click",".list-group-item", function(event){
-       console.log("Item clicked", $(this));
-      //console.log("Output is",$(this)[0].innerText);
       $("#myModal").modal('show');
       $(".body-text > span").text($(this).text());
-          //console.log($("task").text(), $(task).data('link'), $(task).data('description'));
-    console.log("modal body", $(".modal-title"), $(".body-text > span"));
-    $(".modal-title").text($(this).text());
-    $(".body-text > span").text($(this).data('description'));
-    // $(".link").find("span").text($(this).data('link'));
-    $(".link").find("a").attr('href', $(this).data('link'));
-
-      //console.log("ITEM is", $(".body-text"));
+      $(".modal-title").text($(this).text());
+      $(".body-text > span").text($(this).data('description'));
+      $(".link").find("a").attr('href', $(this).data('link'));
     });
-
 });
